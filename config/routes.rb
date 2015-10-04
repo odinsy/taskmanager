21efd9230c591fa953700fb0189c648408502bdc
odinsy <odidoit@gmail.com>
2015-10-04 15:46:56 +0300

    Work on the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,16 @@
 Rails.application.routes.draw do
 
   root 'main#index'
-  # Registrations
+
   resources :registrations, only: [:new, :create]
   get '/sign_up', to: 'registrations#new', as: :sign_up
-  # Sessions
+
   resources :sessions, only: [:new, :create, :destroy]
   get "/login", to: "sessions#new", as: :login
   delete "/logout", to: "sessions#destroy", as: :logout
-  # Projects
-  resources :projects do
-    collection do
-      get :completed, only: [:index]
-    end
-    member do
-      put :run
-      put :complete
-    end
-  end
-  #Tasks
+
+  resources :profile, only: [:edit, :show, :update]
+
   resources :tasks do
     collection do
       get :tomorrow, only: [:index]
@@ -30,6 +22,17 @@ Rails.application.routes.draw do
       put :run
       put :complete
     end
+  end
+
+  resources :projects do
+    collection do
+      get :completed, only: [:index]
+    end
+    member do
+      put :run
+      put :complete
+    end
+    resources :tasks, only: [:new, :create, :destroy], shallow: true
   end
 
 end
