ad7bccb0b2123fc068ee1d488474a004300e204e
odinsy <odidoit@gmail.com>
2015-10-04 15:43:07 +0300

    Added the actions and views for edit and view the profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,23 @@
 class ProfileController < ApplicationController
+
   def show
   end
+
   def edit
   end
+
+  def update
+    if current_user.update_attributes(user_params)
+      redirect_to root_path, notice: "Your profile has been updated!"
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:profile).permit(:email, :password, :password_confirmation)
+  end
+  
 end
