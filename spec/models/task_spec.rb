3eb3fcf544668fa9a28d1478451956d4790e948e
odinsy <odidoit@gmail.com>
2015-10-18 18:34:24 +0300

    Add spec for the model User method downcase_email

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 describe Task do
 
+  context "when user registers" do
+    it "changes his email to downcase" do
+      user = create(:user, email: "MYMAIL@EXAMPLE.COM")
+      expect(user.email).to eq("mymail@example.com")
+    end
+  end
+
 end
